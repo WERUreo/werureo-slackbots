ac030dec09ec89b8514c2a3513c38db9d8c93ae0
Keli'i Martin <kelii.d.martin@gmail.com>
2016-11-18 09:10:46 -0500

    Realm Status slash command
    Adding the first version of the realm status slash command, mainly to test if the Heroku config var for the Battle.net API key works correctly.

--- Sources/App/Controllers/SlackController.swift
+++ Sources/App/Controllers/SlackController.swift
@@ -8,9 +8,30 @@
 
 import Vapor
 import HTTP
+import Transport
+import Foundation
 
 final class SlackController
 {
+    ////////////////////////////////////////////////////////////
+    // MARK: - Properties
+    ////////////////////////////////////////////////////////////
+
+    var drop: Droplet
+
+    ////////////////////////////////////////////////////////////
+    // MARK: - Initializers
+    ////////////////////////////////////////////////////////////
+
+    init(drop: Droplet)
+    {
+        self.drop = drop
+    }
+
+    ////////////////////////////////////////////////////////////
+    // MARK: - Routes
+    ////////////////////////////////////////////////////////////
+
     func alright(request: Request) throws -> ResponseRepresentable
     {
         let attachments = try JSON(node:
@@ -25,5 +46,61 @@ final class SlackController
             ])
         
         return payload
+    }
+
+    ////////////////////////////////////////////////////////////
+
+    func realmstatus(request: Request) throws -> ResponseRepresentable
+    {
+        var uri = "https://us.api.battle.net/wow/realm/status?locale=en_US&apikey="
+
+        if drop.environment == .production
+        {
+            guard let apiKey = getEnvironmentVariable(name: "BATTLENET_API_KEY") else { return "No valid API key" }
+
+            uri.append(apiKey)
+        }
+        else
+        {
+            guard let apiKey = self.drop.config["keys", "battlenet"]?.string else { return "No valid API key" }
+            uri.append(apiKey)
+        }
+
+        guard let text = request.data["text"]?.string else { return "Please enter the name of a World of Warcraft realm." }
+
+        let apiResponse = try BasicClient.get(uri)
+
+        guard let realms = apiResponse.json?["realms"]?.pathIndexableArray else { return "No realms" }
+
+        guard let selectedRealm = realms.filter(
+        { (realm) -> Bool in
+            realm["name"]?.string?.lowercased() == text.lowercased()
+        }).first
+        else
+        {
+            return "\(text) is not a valid realm"
+        }
+
+        let realmName = selectedRealm["name"]?.string ?? "Invalid name"
+        let status = selectedRealm["status"]?.bool ?? false
+
+        let message = "\(realmName) \(status ? "is" : "is not") online."
+        let payload = try JSON(node:
+            [
+                "response_type" : "ephemeral",
+                "text" : message
+            ])
+
+        return payload
+    }
+
+    ////////////////////////////////////////////////////////////
+    // MARK: - Helper functions
+    ////////////////////////////////////////////////////////////
+
+    fileprivate func getEnvironmentVariable(name: String) -> String?
+    {
+        guard let rawValue = getenv(name) else { return nil }
+        return String(utf8String: rawValue)
     }
 }
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,9 +2,12 @@ import Vapor
 
 let drop = Droplet()
 
-let slack = SlackController()
+let slack = SlackController(drop: drop)
 
 // Alright, alright, alright
 drop.get("alright", handler: slack.alright)
+
+// Realm Status
+drop.post("realmstatus", handler: slack.realmstatus)
 
 drop.run()
